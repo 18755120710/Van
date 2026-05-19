d844c838331837eac8f98a5ac495f239c05a2104
butvan <1973578950@qq.com>
2026-05-19 20:59:15 +0800

    feat: 优化前端项目

--- frontend/composables/useView.ts
+++ frontend/composables/useView.ts
@@ -1,9 +1,9 @@
 import { ref } from 'vue'
 
-const currentView = ref<'chat' | 'prompt'>('chat')
+const currentView = ref<'chat' | 'settings'>('chat')
 
 export const useView = () => {
-  const setView = (view: 'chat' | 'prompt') => {
+  const setView = (view: 'chat' | 'settings') => {
     currentView.value = view
   }
   return {
